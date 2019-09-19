9ff1858231b02ca94347a1855640bc4a626eeab8
Beryliy <urenad53@ukr.net>
2019-09-19 23:00:08 +0300

    subscribe on service progress notifications from ExtractFragment

--- app/src/main/java/com/flogiston/test/presentation/extract/ExtractFragment.kt
+++ app/src/main/java/com/flogiston/test/presentation/extract/ExtractFragment.kt
@@ -1,11 +1,16 @@
 package com.flogiston.test.presentation.extract
 
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 
 import com.flogiston.test.R
 
@@ -18,9 +23,21 @@ class ExtractFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
+        registerBroadcastReceiver()
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_extract, container, false)
+    }
+
+    private fun registerBroadcastReceiver(){
+        val localBroadcastManager = LocalBroadcastManager.getInstance(context!!)
+        val intentFilter = IntentFilter()
+        intentFilter.addAction(SHOW_PROGRESS)
+        localBroadcastManager.registerReceiver(object : BroadcastReceiver(){
+            override fun onReceive(context: Context?, intent: Intent?) {
+                //TODO: handle progress notifications
+            }
+        },
+            intentFilter)
     }
 
     companion object {
