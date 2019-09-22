2df60de2f7e8022f252bb7162a7d7ca00af22392
Beryliy <urenad53@ukr.net>
2019-09-22 14:39:31 +0300

    register sensors

--- app/src/main/java/com/flogiston/test/presentation/vr/VRFragment.kt
+++ app/src/main/java/com/flogiston/test/presentation/vr/VRFragment.kt
@@ -1,6 +1,11 @@
 package com.flogiston.test.presentation.vr
 
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -13,14 +18,66 @@ import com.flogiston.test.R
  * A simple [Fragment] subclass.
  */
 class VRFragment : Fragment() {
+    private lateinit var sensorManager : SensorManager
+    private var gyroscope : Sensor? = null
+    private var accelerometer : Sensor? = null
+    private var magnetometer : Sensor? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        sensorManager = requireActivity().getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_vr, container, false)
     }
 
+    override fun onResume() {
+        super.onResume()
+        gyroscope?.also { gyroscope ->
+            sensorManager.registerListener(object : SensorEventListener {
+                override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
 
+                }
+
+                override fun onSensorChanged(event: SensorEvent?) {
+
+                }
+            },
+                gyroscope,
+                SensorManager.SENSOR_DELAY_NORMAL)
+        }
+
+        accelerometer?.also { accelerometer ->
+            sensorManager.registerListener(object : SensorEventListener {
+                override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+                }
+
+                override fun onSensorChanged(event: SensorEvent?) {
+
+                }
+            },
+                accelerometer,
+                SensorManager.SENSOR_DELAY_NORMAL)
+        }
+
+        magnetometer?.also { magnetometer ->
+            sensorManager.registerListener(object : SensorEventListener {
+                override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+                }
+
+                override fun onSensorChanged(event: SensorEvent?) {
+
+                }
+            },
+                magnetometer,
+                SensorManager.SENSOR_DELAY_NORMAL)
+        }
+        }
+    }
 }
